3511722bfd0141d32abf0d54e8af153c2fecf599
futsuhai <130190351+futsuhai@users.noreply.github.com>
2024-02-14 22:12:34 +0300

    init app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'chat';
+
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
+import { APP_ROUTES } from './routes/routes';
+import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
-  providers: []
+  providers: [
+    provideRouter(APP_ROUTES),
+  ]
 };
